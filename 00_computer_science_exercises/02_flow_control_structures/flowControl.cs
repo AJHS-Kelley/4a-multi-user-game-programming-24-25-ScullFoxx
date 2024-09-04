28c79718401cc071c64cd43795b6aaff057a68c0
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-09-04 14:25:21 -0400

    Loops & Iterations

---
+++ 00_computer_science_exercises/02_flow_control_structures/flowControl.cs
@@ -0,0 +1,88 @@
+ // Sculley, Alexandra, 9/04/2024 v0.1
+
+ using System;
+ class Template {
+    static void Main() {
+
+
+
+
+
+
+
+
+
+
+
+        // int day = 1
+        // switch(day)
+        // {
+        //  case 1:
+        //      Console.WriteLine("It's Monday.\n");
+        //      break;
+        //  case 2:
+        //      Console.WriteLine("It's Tuesday.\n");
+        //      break;
+        //  case 3:
+        //      Console.WriteLine("It's Wendesday.\n");
+        //      break;
+        //  case 4:
+        //      Console.WriteLine("It's Thursday.\n");
+        //      break;
+        //  case 5:
+        //      Console.WrtieLine("It's Friday.\n");
+        //      break;
+        //  case 6:
+        //      Console.WriteLine("It's Saturday.\n");
+        //  case 7:
+        //      Console.WriteLine("It's Sunday.\n");
+        //      break;
+        //  
+        // }
+
+        // Repeating Steps over, and over, and over again
+        // Loops are used to repeat instructions multiple times
+
+        // while loops are "musical chairs" in computer science
+        // the code will run as long as the condition is true.
+        // while loop syntax:
+        /* while (CONDITIONAL_EXPRESSION) DOES NOT NEED A COLON
+        {
+            Code to loop goes here
+        }
+        */
+
+        // int z = 0;
+        // while (z <= 0)
+        // {
+        // Console.WriteLine(z)
+        // z++;
+        // }
+
+        int y = 100;
+        while (y != -1)
+        {
+            Console.WriteLine(y);
+            y--;
+        }
+        /*
+        for (Statement1; statement2; statement3)
+        {
+            Code to run through the loop
+        }
+        Statement 1 = code to run before loop starts 1 time!!
+        Statement 2 = conditional expression to check to see each time before loop starts
+        Statement 3 = code to run after each loop
+
+        each 'trip' through the loop is called iteration
+        */
+         
+         // Counting to 10 with loops
+         for (int i = 10; i != -1; i--)
+         {
+            Console.WriteLine(i);
+
+         }
+
+    }
+ }
