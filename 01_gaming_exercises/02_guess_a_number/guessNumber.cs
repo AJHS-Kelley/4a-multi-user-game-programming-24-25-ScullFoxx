a19b71d889c4ed2e94f0e052ea1007a680620ef5
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-09-06 14:21:48 -0400

    added in while loop

--- 01_gaming_exercises/02_guess_a_number/guessNumber.cs
+++ 01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -1,11 +1,31 @@
-// Guess a Number, Sculley, Alexandra, v0.1
+// Guess a Number, Sculley, Alexandra, v0.2
 
 using System;
 class GuessNumber {
     static void Main() {
         int numGuess = 0;
         int maxGuess = 4;
+        int guess;
         // Generate the secret number
+        Random rnd = new Random(); // Create an object named 'rnd' that is a copy of the Random() class.
+        int secretNum = rnd.Next(101); // Generate from 0 to 99
+        // Console.WriteLine(secretNum);
+        // int secretNum = rnd.Next(25, 1000); // Generate from 25 to 999
+        while (numGuess < maxGuess) {
+        
+            Console.WriteLine("Pick a number 0 through 100.");
+            guess = Convert.ToInt32(Console.ReadLine());
+            if (guess == secretNum)
+            {
+                Console.WriteLine("Congrats! You guessed the secret number! The number was: " + secretNum);
+                break;
+            }
+            else if (guess != secretNum)
+            {
+                Console.WriteLine("That's not it! You did not guess the secret number.");
+                numGuess++;
+            }
+        }
         // while loop
             // Allow the user to guess the number.
             // Compare the guess to the secret number.
