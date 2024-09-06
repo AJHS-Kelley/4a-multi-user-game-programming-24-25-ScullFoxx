87510af1d3b71383c61a6ed83ad2b7340f19c4f7
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-09-06 13:57:27 -0400

    num guess

---
+++ 01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -0,0 +1,17 @@
+// Guess a Number, Sculley, Alexandra, v0.1
+
+using System;
+class GuessNumber {
+    static void Main() {
+        int numGuess = 0;
+        int maxGuess = 4;
+        // Generate the secret number
+        // while loop
+            // Allow the user to guess the number.
+            // Compare the guess to the secret number.
+            // If match, print win, if >  or <, give hint.
+            // +1 to numGuess
+            // Check if numGuess > maxGuesses
+            // If true, print a "lose game" message, if false guess again.
+    }
+}
