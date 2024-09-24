f4062ff1b443a505ca9a74a8c0565cbe944d3754
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-09-24 13:57:02 -0400

    Rock Paper Scissors

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rockPaperScissors.cs
@@ -0,0 +1,19 @@
+// Rock Paper Scissors, Sculley Alexandra, v0.0
+
+using System;
+class RPS {
+    static void Main() {
+        // VARIABLES
+        string playerName ="";
+        int playerScore = 0;
+        int cpuScore = 0;
+        string playerChoice = "";
+        string cpuChoice = "";
+
+        // Player Name
+         
+        Console.WriteLine("Welcome to FEESH's Rock, Paper, Scissors\n");
+        Consol
+
+    }
+}
